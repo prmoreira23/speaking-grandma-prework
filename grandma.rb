6cd5e8e489bd4bf9ffa5025dbc9793ae1cfa2817
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-14 20:51:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,4 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(say)
   if say == say.upcase
+      if say == 'I LOVE YOU GRANDMA!'
+        return 'I LOVE YOU TOO PUMPKIN!'
+      else
+        return "NO, NOT SINCE 1938!"
+      end
+  else
+      return 'HUH?! SPEAK UP, SONNY!'
+  end
 end
